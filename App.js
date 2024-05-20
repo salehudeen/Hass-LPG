0eadf1121b9d76c098a22402aed6f0e8b45b006a
salehudeen <salehudeensaleh@gmail.com>
2024-05-20 15:59:34 +0300

    initial commit

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Homepage from './src/screens/homepage/Homepage';
 import LoginForm from './src/screens/Auth/LoginPage';
-import SignupForm from './src/screens/Auth/SignupPage';
+// import SignupForm from './src/screens/Auth/SignupPage';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import { Amplify } from 'aws-amplify';
 import amplifyconfig from './src/amplifyconfiguration.json';
 Amplify.configure(amplifyconfig);
@@ -15,7 +16,7 @@ export default function App() {
     <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen  name="Login" component={LoginForm}/>
-      <Stack.Screen name="Signup" component={SignupForm} />
+      {/* <Stack.Screen name="Signup" component={SignupForm} /> */}
       <Stack.Screen name ="Home" component={Homepage}/>
     </Stack.Navigator>
   </NavigationContainer> 
--- src/screens/Auth/LoginPage.jsx
+++ src/screens/Auth/LoginPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Alert, Button, Image, Pressable, SafeAreaView, StyleSheet, Switch, Text, TextInput, View } from 'react-native'
+
+
 const logo = require("../../assets/Hass-Logo.png")
 
 
